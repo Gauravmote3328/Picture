49cdb77ef24006ae82f614f2c355e0526a0437cc
Gauravmote3328 <gaurav.mote3328@gmail.com>
2023-04-21 00:10:34 +0530

    My Project With Student Complete.

--- Demo/src/Test/A.java
+++ Demo/src/Test/A.java
@@ -1,9 +1,15 @@
 package Test;
 
 public class A {
-	
+
 	public static void main(String[] args) {
 		System.out.println("My Git Project");
+		Student student = new Student();
+		student.setId(1);
+		student.setName("Gaurav");
+		student.setAddress("Pune");
+
+		System.out.println("Student Data :" + student);
 	}
 
 }
