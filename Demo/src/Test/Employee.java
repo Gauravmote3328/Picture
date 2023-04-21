3d34325fc8556687b11f502dd574d9021fa862d6
Mr. Gaurav Mote <123461741+Gauravmote3328@users.noreply.github.com>
2023-04-21 11:12:50 +0530

    Create Employee.java

---
+++ Demo/src/Test/Employee.java
@@ -0,0 +1,25 @@
+public class Employee{
+ 
+  private int id;
+  
+  private String name;
+  
+  public void setId(int id){
+    this.id=id;
+    
+  }
+  
+  public int getId(){
+    return id;
+  }
+  
+  public void setName(String name){
+    this.name=name;
+  }
+  
+  public String getName(){
+    retrun name;
+  }
+  
+  
+}
