36cfdba850d18e80d2a265072df941be6432d5f0
Gauravmote3328 <gaurav.mote3328@gmail.com>
2023-04-21 11:15:38 +0530

    Employee class updated

--- Demo/src/Test/Employee.java
+++ Demo/src/Test/Employee.java
@@ -1,25 +1,31 @@
-public class Employee{
- 
-  private int id;
-  
-  private String name;
-  
-  public void setId(int id){
-    this.id=id;
-    
-  }
-  
-  public int getId(){
-    return id;
-  }
-  
-  public void setName(String name){
-    this.name=name;
-  }
-  
-  public String getName(){
-    retrun name;
-  }
-  
-  
+package Test;
+
+public class Employee {
+
+	private int id;
+
+	private String name;
+
+	public void setId(int id) {
+		this.id = id;
+
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + "]";
+	}
+
 }
